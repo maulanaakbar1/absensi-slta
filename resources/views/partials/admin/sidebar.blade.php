774fe55103143e717c67c11e9e0b8662a6892e1e
maulanaakbar1 <hasbymakbar1@gmail.com>
2026-07-16 20:59:11 +0700

    ganti logo sekolah

--- resources/views/partials/admin/sidebar.blade.php
+++ resources/views/partials/admin/sidebar.blade.php
@@ -21,7 +21,7 @@
                 {{ Auth::user()->role }} Panel
             </p>
         </div>
-
+    
         </div>
         <button @click="sidebarOpen = false" class="md:hidden text-slate-400 hover:text-red-500">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
